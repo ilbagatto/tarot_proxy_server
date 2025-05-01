ee1635879b18d9a8a7517193dd93fe7cd744d426
ilbagatto <sergei.krushinski@gmail.com>
2025-05-01 15:14:28 +0300

    output bufferization overriden

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:tarot_proxy_server/server.dart';
 
 void main() {
+  // Disable stdout buffering for real-time log visibility
+  stdout.nonBlocking;
+  stdout.writeCharCode(0); // optional kickstart
+
   startServer();
 }
